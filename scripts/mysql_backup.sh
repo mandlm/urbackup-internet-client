8efd42599a55cb86d2de892d75c6d8a9808ff50f
Michael Mandl <mandlm@molez.net>
2020-02-03 22:48:03 +0100

    Removed double timestamps in log

--- scripts/mysql_backup.sh
+++ scripts/mysql_backup.sh
@@ -2,12 +2,9 @@
 
 BACKUP_DIR="/backup/database"
 
-echo
 echo "Starting to backup database"
-date
+
 mkdir --parents $BACKUP_DIR
 mysqldump --single-transaction --host=$MYSQL_BACKUP_HOST --user=$MYSQL_BACKUP_USER --password=$MYSQL_BACKUP_PASSWORD $MYSQL_BACKUP_DATABASE > $BACKUP_DIR/$MYSQL_BACKUP_DATABASE.sqldump
 
-echo
 echo "Database backup finished"
-date
